d963d813353165e30f50b14c99af5ebeacbed286
Shiva-2070 <119473288+Shiva-2070@users.noreply.github.com>
2025-07-14 10:30:29 +0530

    dd

---
+++ src/main/java/Cognizant_DeepSkill/File/App.java
@@ -0,0 +1,13 @@
+package Cognizant_DeepSkill.File;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
